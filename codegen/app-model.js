404ba9a4033828b949c0d22f216fd082da301e58
Maurice de Laat <info@mauricedelaat.nl>
2018-09-28 10:44:50 +0200

    Deleted codegen .js output.

--- codegen/app-model.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLW1vZGVsLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiYXBwLW1vZGVsLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
